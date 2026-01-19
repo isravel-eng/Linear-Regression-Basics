85bae599e9ef569ed419ca68fbfc69346ae7f04e
ISRAVEL Y <isravelyosva@gmail.com>
2026-01-19 22:54:20 +0530

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -1,3 +1,22 @@
 # a model 
 def model(m,x,c):
   return m*x+c
+# Day 1
+import matplotlib.pyplot as plt
+
+x=[0,1,2,3,4,5,6,7,8]
+y=[20,40,50,55,60,65,70,80,90]
+
+def predict(m,x,c):
+    return m*x+c
+
+m,c=10,20
+
+y_pred=[predict(m,i,c) for i in x]
+
+plt.subplot(1,2,1)
+plt.plot(x,y)
+plt.subplot(1,2,2)
+plt.plot(x,y_pred)
+plt.savefig("day1_line.png")
+plt.show()
